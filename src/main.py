9f0052314f52a03897513025515ec1e552577b20
Eric García Gómez <garciagomezeric10@gmail.com>
2025-07-07 15:32:27 +0200

    add routers in main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
+from src.routes import book, loan, user
+
 app = FastAPI()
+
+app.include_router(book.router)
+app.include_router(loan.router)
+app.include_router(user.router)
 
 
 @app.get("/")
 def read_root():
     return {"Hola desde la librería creada por Eric García"}
-
