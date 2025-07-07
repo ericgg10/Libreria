c1f34d7880e6d44ae5bf9c1ba192452b3040ef71
Eric García Gómez <garciagomezeric10@gmail.com>
2025-07-07 17:57:05 +0200

    add get_books_attributes and get loans books

--- src/database/book_db.py
+++ src/database/book_db.py
@@ -1,3 +1,6 @@
+from typing import Optional
+from uuid import UUID
+
 from sqlmodel import Session, SQLModel, select
 
 from src.database import engine
@@ -14,3 +17,29 @@ def get_all_books(db: Session):
 
 def get_books_by_id(db: Session, book_id: int):
     return db.get(Book, book_id)
+
+
+def get_loans_book(db: Session, book_id: UUID):
+    query = select(Loan).where(Loan.book_id == book_id)
+    result = db.exec(query).all()
+    return result
+
+
+def get_books_attribute(
+    db: Session,
+    title: Optional[str] = None,
+    author: Optional[str] = None,
+    genre: Optional[str] = None,
+    limit: int = 10,
+):
+    query = select(Book)
+
+    if title:
+        query = query.where(Book.title == title)
+    if author:
+        query = query.where(Book.author == author)
+    if genre:
+        query = query.where(Book.genre == genre)
+    query = query.limit(limit)
+    result = db.exec(query).all()
+    return result
--- src/routes/book.py
+++ src/routes/book.py
@@ -1,6 +1,7 @@
+from typing import Optional
 from uuid import UUID
 
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, Query, status
 
 from src.database import book_db, db_session
 from src.models.books import Book
@@ -15,7 +16,24 @@ def get_all_book(db: db_session):
     return book_db.get_all_books(db)
 
 
-@router.get("/id/{id}")
+@router.get("/search")
+def get_books_attributes(
+    db: db_session,
+    title: Optional[str] = Query(None),
+    author: Optional[str] = Query(None),
+    genre: Optional[str] = Query(None),
+    limit: int = 10,
+):
+    book = book_db.get_books_attribute(db, title=title, author=author, genre=genre, limit=limit)
+    if not book:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No books were found with these attributes",
+        )
+    return book
+
+
+@router.get("/{id}")
 def get_book_with_id(db: db_session, id: UUID):
     book = book_db.get_books_by_id(db, id)
     if not book:
@@ -23,3 +41,14 @@ def get_book_with_id(db: db_session, id: UUID):
             status_code=status.HTTP_404_NOT_FOUND, detail="Book not found with ID {id}"
         )
     return book
+
+
+@router.get("/{id}/loans")
+def get_loans_books(db: db_session, book_id: UUID):
+    loans = book_db.get_loans_book(db, book_id)
+    if not loans:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="No loans were found for this book",
+        )
+    return loans
